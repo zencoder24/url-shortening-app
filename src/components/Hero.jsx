69ff0efe70407e8e18a6beec3ac7ec81e4078180
Tyrell C <tyrellrcurry@gmail.com>
2022-04-04 22:05:06 -0700

    Added Poppins Font to index.html. Added custom properties to Tailwind Config.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function Hero() {
-  return (
-    <div>Hero</div>
-  )
+  return <div className="text-x-base text-x-cyan font-poppins">Hero Test</div>;
 }
 
-export default Hero
+export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'x-cyan': '#2a7d7d',
+        'x-dark-violet': '#353042',
+        'x-red': '#ab1616',
+        'x-gray': '#bfbfbf',
+        'x-gray-violet': '#9995a1',
+        'x-dark-blue': '#333238',
+        'x-dark-violet': '#222124',
+      },
+      fontSize: {
+        'x-base': '18px',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
