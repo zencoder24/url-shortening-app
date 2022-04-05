86137c5c7c2778eaa096d95216c4b0fe46af4ead
Rod Garland <rg6797@outlook.com>
2022-04-04 22:16:48 -0400

    assets and component creation
    Co-authored-by: Tyrell Curry <tyrellrcurry@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-
+import favicon from './assets/images/favicon-32x32.png'
+import ExtraContent from './components/ExtraContent';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      The URL Shortening App!
-    </h1>
+    <>
+      <Navbar/>
+      <Hero/>
+      <SearchBar/>
+      <ExtraContent/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/components/ExtraContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ExtraContent() {
+  return (
+    <div>ExtraContent</div>
+  )
+}
+
+export default ExtraContent
