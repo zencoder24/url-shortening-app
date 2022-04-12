f39076f97a04b356852ae841857a852b3011e4dc
Tyrell C <tyrellrcurry@gmail.com>
2022-04-12 00:49:05 -0700

    ExtraContent created with mobile and desktop responsiveness

--- src/components/ExtraContent/ExtraContent.jsx
+++ src/components/ExtraContent/ExtraContent.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from 'react';
 
 function ExtraContent() {
   return (
-    <div>ExtraContent</div>
-  )
+    <div>
+      <div className="content bg-x-gray">
+        <div className="title text-center px-7 md:px-10 ">
+          <h2 className="text-4xl font-semibold text-x-dark-blue py-4 pt-12 md:text-5xl">
+            Advanced Statistics
+          </h2>
+          <p className="text-x-gray-violet text-x-base md:text-xl xl:text-2xl md:m-auto md:w-4/5 lg:w-3/5 xl:w-2/5">
+            Track how your links are performing accross the web with our
+            advanced statistics dashboard.
+          </p>
+        </div>
+
+        <div className="cards-container">
+          <div className="cards pt-24 py-16 px-12 md:flex">
+            <div className="bg-white text-center relative py-14 rounded-md md:w-full md:max-w-x-1 lg:h-5/6 xl:pb-20">
+              <div className="icon-wrapper bg-x-dark-violet w-20 h-20 flex justify-center items-center rounded-full absolute ml-auto mr-auto left-0 right-0 -mt-24">
+                <div className="icon h-10 w-10 bg-brand-icon bg-contain"></div>
+              </div>
+              <div className="text px-8">
+                <h2 className="font-bold text-2xl text-x-dark-violet pt-2 pb-4 md:text-3xl">
+                  Brand Recognition
+                </h2>
+                <p className="text-x-gray-violet text-x-base md:text-lg lg:text-xl">
+                  Boost your brand recognition with each click. Generic links
+                  don't mean a thing. Branded links help instil confidence in
+                  your content.
+                </p>
+              </div>
+            </div>
+            <div className="h-24 w-2 bg-slate-500 m-auto md:h-2 md:w-1/5"></div>
+            <div className="bg-white text-center relative py-14 rounded-md md:max-w-x-1 xl:mt-8 xl:h-5/6">
+              <div className="icon-wrapper bg-x-dark-violet w-20 h-20 flex justify-center items-center rounded-full absolute ml-auto mr-auto left-0 right-0 -mt-24">
+                <div className="icon h-10 w-10 bg-detailed-icon bg-contain"></div>
+              </div>
+              <div className="text px-8">
+                <h2 className="font-bold text-2xl text-x-dark-violet pt-2 pb-4 md:text-3xl">
+                  Detailed Records
+                </h2>
+                <p className="text-x-gray-violet text-x-base md:text-lg lg:text-xl">
+                  Gain insights into who is clicking your links. Knowing when
+                  and where people engage with your content helps inform better
+                  decisions.
+                </p>
+              </div>
+            </div>
+            <div className="h-24 w-2 bg-slate-500 m-auto md:h-2 md:w-1/5"></div>
+            <div className="bg-white text-center relative py-14 rounded-md md:max-w-x-1 xl:mt-12 lg:pb-24">
+              <div className="icon-wrapper bg-x-dark-violet w-20 h-20 flex justify-center items-center rounded-full absolute ml-auto mr-auto left-0 right-0 -mt-24">
+                <div className="icon h-10 w-10 bg-customize-icon bg-contain"></div>
+              </div>
+              <div className="text px-8">
+                <h2 className="font-bold text-2xl text-x-dark-violet pt-2 pb-4 md:text-3xl">
+                  Fully Customizable
+                </h2>
+                <p className="text-x-gray-violet text-x-base md:text-lg lg:text-xl">
+                  Improve brand awareness and content discoverability through
+                  customizable links, supercharging audience engagement.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="call-to-action">
+          <div className="w-full bg-x-dark-violet bg-boost-mobile bg-cover bg-center text-center">
+            <div className="content py-24 px-6">
+              <h2 className="text-4xl font-semibold text-x-gray pb-10 lg:text-5xl">
+                Boost your links today
+              </h2>
+              <button className="py-5 px-12 bg-x-cyan text-white  rounded-full text-xl font-semibold cursor-pointer hover:bg-cyan-300 xl:text-2xl xl:py-5 xl:px-10">
+                Get Started
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ExtraContent
+export default ExtraContent;
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,18 +21,27 @@ module.exports = {
         'hero-img': "url('/src/assets/images/illustration-working.svg')",
         'search-bg': "url('/src/assets/images/bg-shorten-desktop.svg')",
         'search-mobile': "url('/src/assets/images/bg-shorten-mobile.svg')",
+        'brand-icon': "url('/src/assets/images/icon-brand-recognition.svg')",
+        'detailed-icon': "url('/src/assets/images/icon-detailed-records.svg')",
+        'customize-icon':
+          "url('/src/assets/images/icon-fully-customizable.svg')",
+        boost: "url('/src/assets/images/bg-boost-desktop.svg')",
+        'boost-mobile': "url('/src/assets/images/bg-boost-mobile.svg')",
       },
       spacing: {
         'x-98': '40rem',
         'x-92': '32rem',
         'x-90': '25.5rem',
         'x-5': '5%',
-        'x-6': '',
+        'x-125': '125%',
       },
       backgroundSize: {
         'x-size': '620px',
         'x-size-2': '650px',
         'x-size-3': '900px',
+      },
+      maxWidth: {
+        'x-1': '26rem',
       },
     },
   },
