3815b982ef85c385064ebbc3b2427f323cff8bdf
Rod Garland <rg6797@outlook.com>
2022-04-13 22:51:40 -0400

    Fixed the color of the navbar hamburger icon with text color

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -44,7 +44,7 @@ function Navbar() {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 fill-x-gray"
+          className="h-6 w-6 text-x-gray-violet"
           viewBox="0 0 24 24"
           stroke="currentColor"
           strokeWidth={2}
