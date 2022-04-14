b53b38b85fa5a1e5c53e1cf2fc7e669330a3913c
Rod Garland <rg6797@outlook.com>
2022-04-13 23:12:01 -0400

    Margin fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
 
   return (
     <section className=" bg-x-very-dark-violet px-0 py-12 md:px-16 md:py-20">
-      <div className="flex flex-col gap-8 items-center md:flex-row md:align-top md:items-start md: justify-around">
+      <div className="flex flex-col gap-8 items-center md:flex-row md:items-start md: justify-around">
         <BrandLogo className="fill-white w-24 md:w-24  md:mr-32" />
         <div className="text-center flex flex-col gap-4 md:text-left">
           <h2 className="text-white font-bold ">Features</h2>
@@ -20,7 +20,7 @@ function Footer() {
           </div>
         </div>
         <div className="text-center  flex flex-col gap-4 md:text-left">
-          <h2 className="text-white font-bold mb-2">Resources</h2>
+          <h2 className="text-white font-bold ">Resources</h2>
           <div className='flex flex-col gap-2 md:text-left'>
             <p className="text-x-medium-gray hover:text-x-cyan cursor-pointer">Blog</p>
             <p className="text-x-medium-gray hover:text-x-cyan cursor-pointer">Developers</p>
@@ -28,7 +28,7 @@ function Footer() {
           </div>
         </div>
         <div className="text-center  flex flex-col gap-4 md:text-left">
-          <h2 className="text-white font-bold mb-2 ">Company</h2>
+          <h2 className="text-white font-bold">Company</h2>
           <div className='flex flex-col gap-2 md:text-left'>
             <p className="text-x-medium-gray hover:text-x-cyan cursor-pointer">About</p>
             <p className="text-x-medium-gray hover:text-x-cyan cursor-pointer">Our Team</p>
@@ -36,7 +36,7 @@ function Footer() {
             <p className="text-x-medium-gray hover:text-x-cyan cursor-pointer">Contact</p>
           </div>
         </div >
-        <div className=' mt-6 flex items-center justify-center gap-6 md:mt-0'>
+        <div className=' mt-6 flex items-center justify-center gap-6 md:mt-1'>
           <FacebookIcon className='fill-white hover:fill-x-cyan cursor-pointer' />
           <TwitterIcon className='fill-white hover:fill-x-cyan cursor-pointer' />
           <PinterestIcon className='fill-white hover:fill-x-cyan cursor-pointer' />
